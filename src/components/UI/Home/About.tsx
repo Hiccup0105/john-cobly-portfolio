a11956c9885f5404a8581dbe2547b94cbda4138d
Hiccup0105 <pro.dev.0105@gmail.com>
2025-05-14 16:33:40 +0300

    portfolio

---
+++ src/components/UI/Home/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="flex flex-col gap-2">
+      <p className="m-0 w-full lg:w-2/3">
+      Senior Full Stack Developer with over 10+ years of experience specializing in React, Next, and React-Native for frontend development, alongside extensive experience with Node.js, Express, and GraphQL for back-end services. With over 10 years of experience working on both web and mobile applications, I have successfully delivered projects using React.js, Next.js, React Native and Node.js. 
+      </p>
+      <p className="m-0 w-full lg:w-2/3">
+      My expertise extends to building scalable APIs, real-time data solutions, and cross-platform mobile applications leveraging frameworks like Ionic and Flutter. I am committed to continuous learning and staying up-to-date with the latest technologies.
+      </p>
+    </div>
+  );
+}
+
+export default About;
